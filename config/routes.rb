842a373a26e2df6bdf84f212525caef6bca18748
juan bernal <mesieou@gmail.com>
2024-01-23 09:22:27 +1100

    added the creation of restaurants feature

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
   # root "articles#index"
   get '/restaurants', to: 'restaurants#index'
   get '/restaurants/new', to: 'restaurants#new'
+  post '/restaurants', to: 'restaurants#create'
   get '/restaurants/:id', to: 'restaurants#show', as: :restaurant
 end

ae6f42ce0684b7be9aa2cac00cb8a941356cea12
juan bernal <mesieou@gmail.com>
2024-01-23 11:37:21 +1100

    added reviews controller and new and create methods

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,22 @@
+class ReviewsController < ApplicationController
+  def new
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @review = Review.new
+  end
+
+  def create
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @review = Review.new(review_params)
+    @review.restaurant = @restaurant
+    if @review.valid?
+      @review.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :new, restaurant: @restaurant, status: :unprocessable_entity
+    end
+  end
+
+  def review_params
+    params.require(:review).permit(:content, :rating)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,6 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
-  get '/restaurants', to: 'restaurants#index'
-  get '/restaurants/new', to: 'restaurants#new'
-  post '/restaurants', to: 'restaurants#create'
-  get '/restaurants/:id', to: 'restaurants#show', as: :restaurant
+  resources :restaurants, only: [:index, :new, :create, :show] do
+    resources :reviews, only: [:new, :create]
+  end
 end

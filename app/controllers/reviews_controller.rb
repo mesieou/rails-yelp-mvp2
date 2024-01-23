804eed85a7eafcc5e4aab2e19e1c607e3ac2a6ec
juan bernal <mesieou@gmail.com>
2024-01-23 17:29:15 +1100

    styling

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -8,9 +8,8 @@ class ReviewsController < ApplicationController
     @restaurant = Restaurant.find(params[:restaurant_id])
     @review = Review.new(review_params)
     @review.restaurant = @restaurant
-    if @review.valid?
-      @review.save
-      redirect_to restaurant_path(@restaurant)
+    if @review.save
+      redirect_to restaurant_path(@restaurant), notice: "Review was succesfully created."
     else
       render :new, restaurant: @restaurant, status: :unprocessable_entity
     end
